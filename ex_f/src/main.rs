85596444a1a3479b6e7724cd9e18eee2e2aa3211
venir.dev <luca@venir.dev>
2022-12-07 00:47:41 +0100

    Traits

---
+++ ex_f/src/main.rs
@@ -0,0 +1,50 @@
+trait Bite {
+    fn bite(self: &mut Self);
+}
+
+#[derive(Debug)]
+struct Grapes {
+    amount_left: i32,
+}
+
+impl Bite for Grapes {
+    fn bite(self: &mut Self) {
+        if self.amount_left > 0 {
+            self.amount_left -= 1;
+        }
+    }
+}
+
+fn main() {
+    let mut carrot = Carrot {
+        percent_left: 100.0,
+    };
+    carrot.bite();
+    println!("I take a bite: {:?}", carrot);
+
+    let mut grapes = Grapes { amount_left: 100 };
+    grapes.bite();
+    println!("Eat a grape: {:?}", grapes);
+
+    bunny_nibbles(&mut carrot);
+    println!("Bunny nibbles for awhile: {:?}", carrot);
+}
+
+#[derive(Debug)] // This enables using the debugging format string "{:?}"
+struct Carrot {
+    percent_left: f32,
+}
+
+impl Bite for Carrot {
+    fn bite(self: &mut Self) {
+        // Eat 20% of the remaining carrot. It may take awhile to eat it all...
+        self.percent_left *= 0.8;
+    }
+}
+
+fn bunny_nibbles<T: Bite>(biter: &mut T) {
+    biter.bite();
+    biter.bite();
+    biter.bite();
+    biter.bite();
+}
